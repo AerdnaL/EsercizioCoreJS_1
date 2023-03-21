ffe458d8dfd17600e498b4e56bf67877a647f729
AerdnaL <39456876+AerdnaL@users.noreply.github.com>
2023-03-21 17:13:19 +0100

    Esercizio fatto

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 // crea funzione che si chiede se la stringa è conenuta all'interno di autore e titolo
 // ricerca case insensitive, anche se cerco Divina Commedia deve trovare divina commedia
 
-// Soluzione ChatGPT
+const autore = "Dante Alighieri";
+const titolo = "La Divina Commedia";
+const stringa1 = "divina";
+const stringa2 = "promessi";
+const stringa3 = "CoMmEdIa";
+
+function stringaInTitolo (autore, titolo, stringa) {
+  const autoreTitolo = `${autore} ${titolo}`.toLowerCase();
+  return autoreTitolo.includes(stringa.toLowerCase());
+}
+
+console.log(stringaInTitolo(autore, titolo, stringa1));
+console.log(stringaInTitolo(autore, titolo, stringa2));
+console.log(stringaInTitolo(autore, titolo, stringa3));
+
+/* Soluzione ChatGPT
 const author = "J.K. Rowling";
 const title = "Harry Potter and the Philosopher's Stone";
 const string1 = "philosopher";
@@ -14,3 +29,4 @@ function checkStringInTitleAndAuthor(author, title, string) {
 
 console.log(checkStringInTitleAndAuthor(author, title, string1)); // Output: true
 console.log(checkStringInTitleAndAuthor(author, title, string2)); // Output: false
+*/
